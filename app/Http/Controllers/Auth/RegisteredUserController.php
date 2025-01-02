55663bafbae65bb14a862e361b4bc444b502d1b3
timotiusdwiyuliantomalau <timotiusdym17@gmail.com>
2025-01-02 15:14:51 +0700

    Update

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -36,7 +36,7 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
+        // Auth::login($user);
 
         return response()->noContent();
     }
